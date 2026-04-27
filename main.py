fb242b69eb54180c6ba8f1f90d379007b511d176
Focus-Believe <focusandbelieve50@gmail.com>
2026-04-28 01:31:01 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,105 @@
+import json, os
+from datetime import datetime
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+from db import DB
+from manager import Manager
+
+app = FastAPI()
+db = DB()
+mgr = Manager()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+async def send_users():
+    users = mgr.users()
+    for ws in list(mgr.name_to_ws.values()):
+        try:
+            await ws.send_text(json.dumps({
+                "type": "users",
+                "data": users
+            }))
+        except:
+            mgr.disconnect(ws)
+
+
+@app.websocket("/ws")
+async def ws(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        while True:
+            data = json.loads(await websocket.receive_text())
+            t = data.get("type")
+
+            if t == "register":
+                ok = db.register(data.get("name"), data.get("password"))
+                await websocket.send_text(json.dumps({
+                    "type": "register_ok" if ok else "register_fail"
+                }))
+
+            elif t == "login":
+                ok = db.login(data.get("name"), data.get("password"))
+
+                if ok:
+                    await mgr.connect(data.get("name"), websocket)
+                    await send_users()
+
+                await websocket.send_text(json.dumps({
+                    "type": "login_ok" if ok else "login_fail"
+                }))
+
+            elif t == "dm":
+                sender = mgr.get_name(websocket)
+                target = mgr.get_ws(data.get("to"))
+
+                if not sender:
+                    continue
+
+                time = datetime.now().strftime("%H:%M")
+
+                db.save_msg(sender, data.get("to"), None, data.get("msg"), time)
+
+                if target:
+                    await target.send_text(json.dumps({
+                        "type": "msg",
+                        "from": sender,
+                        "msg": data.get("msg"),
+                        "time": time
+                    }))
+
+            elif t == "room":
+                sender = mgr.get_name(websocket)
+
+                if not sender:
+                    continue
+
+                time = datetime.now().strftime("%H:%M")
+
+                db.save_msg(sender, None, data.get("room"), data.get("msg"), time)
+
+                msg = json.dumps({
+                    "type": "room",
+                    "from": sender,
+                    "msg": data.get("msg"),
+                    "time": time
+                })
+
+                for ws_client in list(mgr.name_to_ws.values()):
+                    try:
+                        await ws_client.send_text(msg)
+                    except:
+                        mgr.disconnect(ws_client)
+
+    except WebSocketDisconnect:
+        mgr.disconnect(websocket)
+        await send_users()
